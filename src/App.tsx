3e6b234c3f23041602c347dac705628bb1d95444
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-06-21 07:27:20 +0400

    git init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>2</p>
+    </>
+  )
+};
+
+export default App
